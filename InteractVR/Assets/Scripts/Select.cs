d24eed25074e41ec15b1378e8889c2e552485891
Dakota <tabston1@vols.utk.edu>
2017-02-05 13:25:49 -0500

    Added Select script

---
+++ InteractVR/Assets/Scripts/Select.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour {
+
+    GameObject camera;
+    Transform cameraTrans;
+
+	// Use this for initialization
+	void Start () {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        Physics.Raycast()
+
+	}
+}
