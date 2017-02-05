157516353f81209c1f80bad4b9c469742b5dfcaf
Dakota <tabston1@vols.utk.edu>
2017-02-05 14:13:06 -0500

    Added basic functionality to Select script

--- InteractVR/Assets/Scripts/Select.cs
+++ InteractVR/Assets/Scripts/Select.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class Select : MonoBehaviour {
 
     GameObject camera;
+    GameObject hitObject;
     Transform cameraTrans;
+    RaycastHit hit;
 
 	// Use this for initialization
 	void Start () {
         camera = GameObject.FindGameObjectWithTag("MainCamera");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-        Physics.Raycast()
+        cameraTrans = camera.GetComponent<Transform>();
+    }
 
-	}
+    void Update()
+    {
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            Debug.DrawRay(cameraTrans.position, cameraTrans.forward * 10, Color.green);
+            if (Physics.Raycast(cameraTrans.position, cameraTrans.forward, out hit, 10))
+            {
+                //Debug.DrawRay(cameraTrans.position, cameraTrans.forward * 20, Color.green);
+                Debug.Log("Hit an Object");
+                hitObject = hit.transform.gameObject;
+                Debug.Log(hitObject.name);
+            }
+
+        }
+    }
 }
