2f7f69c41373fdf15b90f2a8cddad95d87d1b60c
Dakota <tabston1@vols.utk.edu>
2017-02-04 23:04:30 -0500

    Added navigation script

---
+++ InteractVR/Assets/Scripts/Navigate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Navigate : MonoBehaviour {
+
+    public float speed;
+    private GvrHead head;
+
+	//Sets the speed of the movement and grabs the head object for referencing the persons gaze direction
+	void Start () {
+        speed = 0.5f;
+        head = FindObjectOfType<GvrHead>();
+    }
+	
+    //Continuously check for the navigation button being pressed
+	void Update () {
+
+        //Moves the camera forward in the direction you are looking
+        if (Input.GetButton("Fire3"))
+        {
+            this.transform.position += speed * head.Gaze.direction;             
+        }
+
+	}
+}
