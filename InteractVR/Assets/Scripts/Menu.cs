a28648f77da87d067e2f167613888accf33921e9
ahuber6 <anthony.huber6@gmail.com>
2017-02-20 15:17:01 -0500

    Added menu functionality

---
+++ InteractVR/Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public Canvas menu;
+    private bool isActive;
+
+	// Use this for initialization
+	void Start () {
+        isActive = false;
+        menu.transform.position = menu.transform.parent.transform.position;
+        menu.transform.position = new Vector3(menu.transform.position.x, menu.transform.position.y, menu.transform.position.z + 1f);
+        menu.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetButtonDown("Submit")) menuButton();
+
+	}
+
+    void menuButton()
+    {
+        menu.gameObject.SetActive(!isActive);
+        isActive = !isActive;
+    }
+}
