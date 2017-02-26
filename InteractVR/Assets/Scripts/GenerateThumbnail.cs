2101bd18ccae85740b7d6608deefc263ab422f1b
Dakota <tabston1@vols.utk.edu>
2017-02-26 18:31:48 -0500

    Added texture for bed button

---
+++ InteractVR/Assets/Scripts/GenerateThumbnail.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GenerateThumbnail : MonoBehaviour {
+
+    object currentObject;
+
+
+	// Use this for initialization
+	void Start () {
+        currentObject = this;
+       
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+}
