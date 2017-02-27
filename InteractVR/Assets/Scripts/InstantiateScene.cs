6d77b6f7280a057d9b24722f31bdeb9f6c1997de
Dakota <tabston1@vols.utk.edu>
2017-02-27 00:19:23 -0500

    Added more objects to the catalog menu

--- InteractVR/Assets/Scripts/InstantiateScene.cs
+++ InteractVR/Assets/Scripts/InstantiateScene.cs
@@ -5,27 +5,43 @@ using UnityEngine;
 public class InstantiateScene : MonoBehaviour {
 
     UnityEngine.WWW www;
+    string []names;
 
     /*
     void Start()
     {
-        if (Application.platform == RuntimePlatform.WindowsEditor)
-        {
-            www = new WWW("https://s3.amazonaws.com/immersacad-storage/demos/mars/5846ce4637518_Android.unity3d");
-            Instantiate(www.assetBundle.mainAsset);
-        }
+     
     }
     */
     IEnumerator Start()
     {
         Debug.Log("Outside");
-       // GameObject l = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-       // Instantiate(l);
+        // GameObject l = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        // Instantiate(l);
+
+        www = UnityEngine.WWW.LoadFromCacheOrDownload("https://s3.amazonaws.com/immersacad-storage/demos/mars/5846ce4637518_Android.unity3d", 0, 0);
+        //www = new WWW("https://s3.amazonaws.com/immersacad-storage/demos/mars/5846ce4637518_Android.unity3d");
+        yield return www;
+        if (www == null)
+        {
+            Debug.Log("Didnt load anything.");
+        }
+        names = www.assetBundle.GetAllAssetNames();
+        foreach (string element in names)
+        {
+            Debug.Log(element);
+        }
+
         if (Application.platform == RuntimePlatform.Android)
         {
-            www = UnityEngine.WWW.LoadFromCacheOrDownload("https://s3.amazonaws.com/immersacad-storage/demos/mars/5846cd4637518_Android.unity3d", 0, 0);
-            yield return www;
-            Instantiate(www.assetBundle.mainAsset, new Vector3(0, 45, 0), new Quaternion(0, 0, 0, 0));
+           
+
+
+            if (www != null)
+            {
+                
+            }
+            //Instantiate(www.assetBundle.mainAsset, new Vector3(0, 45, 0), new Quaternion(0, 0, 0, 0));
             
             //www = new WWW("https://s3.amazonaws.com/immersacad-storage/demos/mars/5846ce4637518_Android.unity3d");
             //yield return www;
