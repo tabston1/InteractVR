a031369caa17128602c146865a46fb9c84f3a54a
Dakota <tabston1@vols.utk.edu>
2017-02-26 19:57:36 -0500

    Added functionality for instantiating actual models

---
+++ InteractVR/Assets/Scripts/Buttons/Catalog/CreateObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+
+    private GameObject camera;
+    GameObject bed;
+
+    void onClick()
+    {
+        camera = GameObject.Find("Main Camera");
+        bed = (GameObject)Instantiate(Resources.Load(this.name));
+        bed.transform.position = camera.transform.position + (5 * camera.transform.forward);
+
+    }
+
+}
