3256d190d791d597d1b16743b6eb28d3402aa4f4
Rob-Ferguson <robbymferguson@gmail.com>
2017-03-26 17:54:04 -0400

    Fixed bug where pressing ObjectDelete button on Billboard would not trigger laser pointer color to change from green to red

--- InteractVR/Assets/Scripts/Buttons/ObjectDelete.cs
+++ InteractVR/Assets/Scripts/Buttons/ObjectDelete.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ObjectDelete : MonoBehaviour
 {
-
 	private GameObject parent;
 
 	void onClick ()
 	{
 		Destroy (this.transform.parent.transform.parent.gameObject);
 
-
+		//Emulate the "OnGazeLeave()" functionality from the Select script
+		if (Manager.select != null) {
+			EventSystem.current.SetSelectedGameObject (null);
+			Manager.select.lineColor (Color.red, Color.red);
+		}
 	}
-
-
 }
