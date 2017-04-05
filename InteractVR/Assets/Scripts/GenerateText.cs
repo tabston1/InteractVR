f522f400b01ba90d5c05a137511dc158cb2c3049
Dakota <tabston1@vols.utk.edu>
2017-04-05 10:27:59 -0400

    added save and load functionality

--- InteractVR/Assets/Scripts/GenerateText.cs
+++ InteractVR/Assets/Scripts/GenerateText.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GenerateText : MonoBehaviour {
 
     protected GameObject parentObject;
     protected GameObject billboard;
-    protected Component textComponent;
-    protected GUIText text;
+    protected GameObject information;
+    protected Text text;
     protected float []scale;
-    protected int[] rotation;
+    protected float[] rotation;
     protected int[] worldRotation;
+    protected string buildNo;
 
 	// Use this for initialization
 	void Start () {
-        billboard = this.gameObject;
+        billboard = this.transform.parent.gameObject;
         parentObject = billboard.transform.root.gameObject;
-        textComponent = billboard.AddComponent<GUIText>();
-        text = textComponent.GetComponent<GUIText>();
-        rotation = new int[3];
+        information = this.gameObject;
+        text = information.AddComponent<Text>();
+        buildNo = parentObject.GetComponent<BasicObject>().buildNo;
+
+        //Set the text attributes
+        text.font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        text.color = Color.black;
+        text.fontSize = 2;
+
+        //initialize the default scale and rotation values
+        rotation = new float[3];
         worldRotation = new int[3];
         scale = new float[3];
         scale[0] = parentObject.transform.localScale.x;
@@ -27,14 +37,14 @@ public class GenerateText : MonoBehaviour {
         rotation[0] = 0;
         rotation[1] = 0;
         rotation[2] = 0;
-        worldRotation[0] = (int)Mathf.Round(parentObject.transform.localRotation.x);
-        worldRotation[1] = (int)Mathf.Round(parentObject.transform.localRotation.y);
-        worldRotation[2] = (int)Mathf.Round(parentObject.transform.localRotation.z);
+        worldRotation[0] = (int)parentObject.transform.localRotation.x;
+        worldRotation[1] = (int)parentObject.transform.localRotation.y;
+        worldRotation[2] = (int)parentObject.transform.localRotation.z;
         
         
         //Initialize the text when the object is first instantiated
-        text.text = "Object: " + billboard.name + "\n" +
-                    "Scale: " + scale + "\n" +
+        text.text = "Object: " + buildNo + "\n" +
+                    "Scale: " + "\n" +
                     "    X = " + scale[0] + "\n" +
                     "    Y = " + scale[1] + "\n" +
                     "    Z = " + scale[2] + "\n" +
@@ -42,27 +52,32 @@ public class GenerateText : MonoBehaviour {
                     "    X = " + rotation[0] + "\n" +
                     "    Y = " + rotation[1] + "\n" +
                     "    Z = " + rotation[2] + "\n";
+       
 
     }
 	
     void updateRelativeRotation()
     {
-        rotation[0] = (int)Mathf.Round(parentObject.transform.rotation.x - worldRotation[0]);
-        rotation[1] = (int)Mathf.Round(parentObject.transform.rotation.y - worldRotation[1]);
-        rotation[2] = (int)Mathf.Round(parentObject.transform.rotation.z - worldRotation[2]);
-
+        //rotation[0] = (int)parentObject.transform.localRotation.x - worldRotation[0];
+        //rotation[1] = (int)parentObject.transform.localRotation.y - worldRotation[1];
+        //rotation[2] = (int)parentObject.transform.localRotation.z - worldRotation[2];
+       
     }
 
 
-    void updateText()
+    public void updateText()
     {
-        updateRelativeRotation();
+        
+        rotation[0] = parentObject.transform.rotation.x;
+        rotation[1] = parentObject.transform.rotation.y;
+        rotation[2] = parentObject.transform.rotation.z;
+
         scale[0] = parentObject.transform.localScale.x;
         scale[1] = parentObject.transform.localScale.y;
         scale[2] = parentObject.transform.localScale.z;
 
         text.text = "Object: " + billboard.name + "\n" +
-                    "Scale: " + scale + "\n" +
+                    "Scale: " + "\n" +
                     "    X = " + scale[0] + "\n" +
                     "    Y = " + scale[1] + "\n" +
                     "    Z = " + scale[2] + "\n" +
@@ -71,6 +86,7 @@ public class GenerateText : MonoBehaviour {
                     "    Y = " + rotation[1] + "\n" +
                     "    Z = " + rotation[2] + "\n";
 
+        Debug.Log("updating text");
     }
 
 }
